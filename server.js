a3476b0c8fd5f6ae4210381857be4e3b75fcfb9f
Jerica Mitchell <jericam32@gmail.com>
2019-11-06 11:43:03 -0800

    ports

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const superagent = require('superagent');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.use(cors());
 
 let locations = {};
